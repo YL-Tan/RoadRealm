124f24f9dcc41b0ac97594827740e889756aba02
YL-Tan <77035911+YL-Tan@users.noreply.github.com>
2023-04-15 13:45:11 -0700

    Removed block class, added Grid class

--- 4-Demo-MouseTest.cpp
+++ 4-Demo-MouseTest.cpp
@@ -4,66 +4,114 @@
 #include <glfw3.h>
 #include "Draw.h"
 #include "GLXtras.h"
+#include <vector>
 
-class Block {
+class Grid {
 public:
-	int x, y, width, height;
-	vec3 color;
-	Block(int x, int y, int width, int height, vec3 color) : x(x), y(y), width(width), height(height), color(color) { }
+    int resolution;
+    int cellSize;
+    int winWidth, winHeight;
+
+    Grid(int resolution, int winWidth, int winHeight) : resolution(resolution), winWidth(winWidth), winHeight(winHeight) {
+        UpdateCellSize();
+    }
+
+    void UpdateCellSize() {
+        int newCellSize = std::min(winWidth, winHeight) / resolution;
+        cellSize = (newCellSize > 0) ? newCellSize : 1; // make sure cellSize is at least 1
+    }
+
+    void Resize(int width, int height) {
+        winWidth = width;
+        winHeight = height;
+        UpdateCellSize();
+    }
+
+    void Draw() {
+        vec3 color(0, 0, 0); // black
+        int horizontalLines = winHeight / cellSize + 1; // + 1 to add a boundary line
+        int verticalLines = winWidth / cellSize + 1;    // + 1 to add a boundary line
+
+        for (int i = 0; i <= horizontalLines; i++) {
+            Line(vec2(0, i * cellSize), vec2(winWidth, i * cellSize), 1.0f, color);
+        }
+        for (int i = 0; i <= verticalLines; i++) {
+            Line(vec2(i * cellSize, 0), vec2(i * cellSize, winHeight), 1.0f, color);
+        }
+    }
 };
 
-int winWidth = 400, winHeight = 400;
-Block block(100, 100, 100, 50, vec3(1, 0, 0));
+int windowedX = 200, windowedY = 200, windowedWidth = 600, windowedHeight = 600;
+int RESOLUTION = 64;
+Grid grid(RESOLUTION, 0, 0);        // Initialize with 0, will be updated in Resize
 vec3 backColor(0, .5f, 0);
-bool blockPicked = false;
-vec2 mouseRef;
-
-void DrawBlock(Block b) {
-	Quad(b.x, b.y, b.x+b.width, b.y, b.x+b.width, b.y+b.height, b.x, b.y+b.height, true, b.color);
-}
+GLFWmonitor* primaryMonitor = nullptr;
+GLFWwindow* w = nullptr;
+bool isFullscreen = false;
+std::vector<std::vector<bool>> gridState;
 
 void Display() {
-	glClearColor(backColor.x, backColor.y, backColor.z, 1);	// set background color
-	glClear(GL_COLOR_BUFFER_BIT);							// clear background and z-buffer
-	UseDrawShader(ScreenMode());
-	DrawBlock(block);
-	glFlush();
+    glClearColor(backColor.x, backColor.y, backColor.z, 1);    // set background color
+    glClear(GL_COLOR_BUFFER_BIT);                            // clear background and z-buffer
+    UseDrawShader(ScreenMode());
+    grid.Draw();
+    glFlush();
 }
 
-bool BlockHit(float mouseX, float mouseY, Block b) {
-	return mouseX >= b.x && mouseX <= b.x+b.width && mouseY >= b.y && mouseY <= b.y+b.height;
+void Resize(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+    grid.Resize(width, height);
 }
 
-void MouseButton(float x, float y, bool left, bool down) {
-	blockPicked = down && BlockHit(x, y, block);
-	if (blockPicked)
-		mouseRef = vec2(x, y);
+void ToggleFullscreen() {
+    if (!isFullscreen) {
+        glfwGetWindowPos(w, &windowedX, &windowedY);    // Store the current window position and size
+        glfwGetWindowSize(w, &windowedWidth, &windowedHeight);
+
+        const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+        glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+        glfwSetWindowMonitor(w, primaryMonitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+    }
+    else {
+        glfwSetWindowMonitor(w, NULL, windowedX, windowedY, windowedWidth, windowedHeight, GLFW_DONT_CARE);
+    }
+    isFullscreen = !isFullscreen;
 }
 
-void MouseMove(float x, float y, bool leftDown, bool rightDown) {
-	if (leftDown && blockPicked) {
-		vec2 mouse(x, y), dif = mouse-mouseRef;
-		block.x += (int) dif.x;
-		block.y += (int) dif.y;
-		mouseRef = mouse;
-	}
+void KeyCallback(GLFWwindow* w, int key, int scancode, int action, int mods) {
+    if (action == GLFW_PRESS)
+        if (key == GLFW_KEY_F)
+            ToggleFullscreen();
 }
 
-void Resize(int width, int height) {
-	glViewport(0, 0, winWidth = width, winHeight = height);
+GLFWwindow* InitGLFW(int x, int y, const char* title) {
+    if (!glfwInit())
+        return NULL;
+    primaryMonitor = glfwGetPrimaryMonitor();
+    GLFWwindow* w = glfwCreateWindow(windowedWidth, windowedHeight, title, NULL, NULL);
+
+    glfwMakeContextCurrent(w);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    return w;
 }
 
-const char *usage = R"(
-	mouse-click on block to pick
-	mouse-drag to move block
+const char* usage = R"(
+	Press F to toggle the view mode from the windowed mode to fullscreen
 )";
 
 int main() {
-	GLFWwindow *w = InitGLFW(200, 200, 600, 600, "Mouse Test");
-	printf("Usage:%s", usage);
-	RegisterMouseMove(MouseMove);
-	RegisterMouseButton(MouseButton);
-	RegisterResize(Resize);
+	// GLFWwindow *w = InitGLFW(200, 200, 600, 600, "Mouse Test");
+    w = InitGLFW(windowedX, windowedY, "Mouse Test");
+    printf("Usage:%s", usage);
+    
+	int width, height;
+	glfwGetFramebufferSize(w, &width, &height); // Get the initial window size
+	Resize(w, width, height); // initialize the grid
+    
+    glfwSetKeyCallback(w, KeyCallback); // Register the key callback
+    //RegisterMouseButton(MouseButton);
+    
+	glfwSetFramebufferSizeCallback(w, Resize);
 	glfwSwapInterval(1);
 	while (!glfwWindowShouldClose(w)) {
 		Display();
